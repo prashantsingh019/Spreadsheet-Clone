7a466cdfde0391af8ba55983c8a101993aba5e10
Prashant Singh <ps731278@gmail.com>
2025-07-01 17:06:31 +0530

    Built formula Bar

---
+++ src/component/FormulaBar.tsx
@@ -0,0 +1,43 @@
+
+
+function FormulaBar() {
+  return (
+        <section className="h-[5%] pl-6 flex justify-between">
+          <div className="left bg-[#E2E2E2] w-[50%]">
+            <div className="formula-bar flex items-center gap-2 ">
+              <div className="tab flex items-center gap-2 bg-[#EEEEEE] border border-[#EEEEEE]">
+                {" "}
+                <img src="./Link.svg" /> Q3 Financial Overview
+              </div>
+              <img src="./Arrow Sync.svg" />
+            </div>
+          </div>
+          <div className="right flex items-center">
+            <div className="flex items-center gap-2 bg-[#D2E0D4] px-4">
+              <img src="/Arrow Split (1).svg" />
+              ABC
+              <img src="/More.svg" />
+            </div>
+
+            <div className="flex items-center gap-2 bg-[#DCCFFC]  px-4">
+              <img src="/Arrow Split (2).svg" />
+              Answer a question
+              <img src="/More.svg" />
+            </div>
+
+            <div className="flex items-center gap-2 bg-[#FAC2AF]  px-4">
+              <img src="/Arrow Split (2).svg" />
+              Extract
+                <img src="/More.svg" />
+              </div>
+            
+            <div className="bg-[#CBCBCB] px-2 py-[2px]">
+              <img src="/Add.svg" />
+            </div>
+           
+          </div>
+        </section>
+  )
+}
+
+export default FormulaBar
