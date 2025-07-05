99f5c7ba45b6f5596af5a11eb45af89334e356ad
Prashant Singh <ps731278@gmail.com>
2025-07-05 08:13:45 +0530

    Built sheet bar end to end

--- src/component/SheetBar.tsx
+++ src/component/SheetBar.tsx
@@ -1,6 +1,42 @@
-function SheetBar(){
-  return <div className="h-[5%] w-full"></div>
-}
+import SheetTab from "./SheetTab";
 
+type sheetProps = {
+  sheets: string[];
+  setSheets: React.Dispatch<React.SetStateAction<string[]>>;
+  activeSheet: number;
+  setActiveSheet: React.Dispatch<React.SetStateAction<number>>;
+};
+
+function SheetBar({
+  sheets,
+  setSheets,
+  activeSheet,
+  setActiveSheet,
+}: sheetProps) {
+  const handleAddSheet = () => {
+    const name = `sheet ${sheets.length + 1}`;
+    setSheets((prev) => [...prev, name]);
+  };
+
+  return (
+    <div className="h-[5%] w-full border border-red-900 flex items-center p-0.5">
+      {sheets.map((sheet, index) => {
+        return (
+          <SheetTab
+            key={index}
+            name={sheet}
+            index={index}
+            setSheets={setSheets}
+            activeSheet={activeSheet}
+            setActiveSheet={setActiveSheet}
+          />
+        );
+      })}
+      <button onClick={handleAddSheet} className="flex items-center px-1">
+        <img src="./Add.svg" />
+      </button>
+    </div>
+  );
+}
 
 export default SheetBar;
