014b8c3398e5a495a317a4d9f6e53ad43cbd5db6
Prashant Singh <ps731278@gmail.com>
2025-07-07 16:34:11 +0530

    minor change css

--- src/component/SpreadSheetBody.tsx
+++ src/component/SpreadSheetBody.tsx
@@ -3,6 +3,7 @@ import Sheet from "./Sheet"
 function SpreadSheetBody() {
   return (
     <div className="h-[70.5%] w-full border border-red-900 mb-0">
+         
         <Sheet/>
         {/* <Sheet/> */}
     </div>
